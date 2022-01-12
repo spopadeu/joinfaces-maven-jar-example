31fa46c39ff127dcf87a17b122fc4c011637d8ad
popeye <stefan.popa@posteo.net>
2022-01-12 15:56:50 +0100

    Pagination and Sort with Datatable and JPA

--- src/main/java/org/joinfaces/example/view/EmployeeBean.java
+++ src/main/java/org/joinfaces/example/view/EmployeeBean.java
@@ -2,28 +2,29 @@ package org.joinfaces.example.view;
 
 
 import lombok.Getter;
-import org.joinfaces.example.entities.Employee;
 import org.joinfaces.example.repositories.EmployeeRepository;
-import org.joinfaces.example.services.DataService;
-import org.primefaces.model.LazyDataModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.view.ViewScoped;
-import java.util.List;
+import javax.inject.Inject;
 
 @Component
 @ViewScoped
 @Getter
 public class EmployeeBean {
-    EmployeeLazyDataModel dataModel = new EmployeeLazyDataModel();
 
-    public LazyDataModel<Employee> getModel(){
-        return dataModel;
+    private EmployeeLazyDataModel dataModel;
+
+    private EmployeeRepository employeeRepository;
+
+    @Inject
+    public EmployeeBean(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+        this.dataModel = new EmployeeLazyDataModel(employeeRepository);
     }
 
-//    private List<Employee> employeeList;
+    //    private List<Employee> employeeList;
 //    @Autowired
 //    EmployeeRepository employeeRepository;
 //
--- src/main/java/org/joinfaces/example/view/EmployeeLazyDataModel.java
+++ src/main/java/org/joinfaces/example/view/EmployeeLazyDataModel.java
@@ -5,32 +5,70 @@ import org.joinfaces.example.repositories.EmployeeRepository;
 import org.primefaces.model.FilterMeta;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortMeta;
+import org.primefaces.model.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
-
-public class EmployeeLazyDataModel  extends LazyDataModel<Employee> {
+public class EmployeeLazyDataModel extends LazyDataModel<Employee> {
 
 
     private EmployeeRepository employeeRepository;
 
-    public EmployeeLazyDataModel(){
+    public EmployeeLazyDataModel(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
         this.setRowCount((int) employeeRepository.count());
     }
 
     @Override
-    public List<Employee> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
-        Pageable firstPageWithTwoElements = PageRequest.of(first, pageSize);
-
-        Page <Employee> list = employeeRepository.findAll(firstPageWithTwoElements);
+    // PF Datatable
+    public List<Employee> load(int offset, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
+        int page = offset / pageSize;
+        // Spring Data JPA
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Sort sort = null;
+        Page<Employee> list;
+        if (!sortBy.isEmpty()) {
+            String first = sortBy.keySet().stream().findFirst().get();
+            sort = Sort.by(first);
+            SortMeta sortMeta = sortBy.get(first);
+            if (sortMeta.getOrder().isDescending()) {
+                sort = sort.descending();
+            } else {
+                sort = sort.ascending();
+            }
+            list = employeeRepository.findAll(PageRequest.of(page, pageSize,sort));
+        } else {
+            list = employeeRepository.findAll(PageRequest.of(page, pageSize));
+        }
         return list.toList();
     }
 
+//    @Override
+//    public List<Employee> load(int offset, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy){
+//        int page = offset / pageSize ;
+//
+//        Sort sort= Sort.by(sortField);
+//        if(sortOrder.isDescending()){
+//            sort=sort.descending();
+//        }else{
+//            sort=sort.ascending();
+//        }
+//
+//
+//        // Spring Data JPA
+//        Pageable pageable = PageRequest.of(page, pageSize, sort);
+//
+//        Page <Employee> list = employeeRepository.findAll(pageable);
+//        return list.toList();
+//    }
 
 }
